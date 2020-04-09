3fc922ca1f16610cb4a718548457b3fee25a1f31
Alvin Coulson <aaacoulson@gmail.com>
2020-04-09 16:06:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,9 @@ require 'active_support/inflector'
 class Song
 
 
-  # def self.table_name
-  #   self.to_s.downcase.pluralize
-  # end
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
 
   def self.column_names
     DB[:conn].results_as_hash = true
